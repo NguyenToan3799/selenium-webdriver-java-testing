e7b1b2933585afd6240e199be72ec6a3c2a066f6
Toan Nguyen <nstoan371999@gmail.com>
2026-03-21 15:49:17 +0700

    Initial Frist Commit

--- selenium/webdriver/CheckEnvironmentTest.java
+++ selenium/webdriver/CheckEnvironmentTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
-public class Topic_01_Check_EnvironmentTest {
+public class CheckEnvironmentTest {
     WebDriver driver;
 
     @Test
